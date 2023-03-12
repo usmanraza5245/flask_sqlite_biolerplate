836fe4235b5999349e59413f95a56894a11826eb
ratroot92 <zcomisb.007@gmail.com>
2023-03-12 05:37:55 +0500

    if this commit gets merged it will complete all basic modules

--- utils/snsscrapper.py
+++ utils/snsscrapper.py
@@ -1,16 +1,70 @@
+from bson.objectid import ObjectId
 import snscrape.modules.twitter as sntwitter
+from config.db import db
 
 
 class Scrapper:
+    def __init__(self) -> None:
+        pass
 
+    @staticmethod
     def scrapKeywords(target):
         if (target['targetType'] == 'keywords'):
-            tweets = []
-            snsScrapper = sntwitter.TwitterSearchScraper(
-                'COVID Vaccine since:2021-01-01 until:2021-05-31')
-            for counter, tweet in enumerate(snsScrapper.get_items()):
-                if counter > 10:
-                    break
-                tweets.append(
-                    [tweet.date, tweet.id, tweet.content, tweet.user.username])
-            print(tweets)
+
+            for keyword in eval(target['targets']):
+                tweets = []
+                snsScrapper = sntwitter.TwitterSearchScraper(keyword)
+                for counter, tweet in enumerate(snsScrapper.get_items()):
+                    if counter > target['limit  ']:
+                        break
+                    tweets.append({
+                        'keyword': keyword,
+                        'date': tweet.date,
+                        'id': tweet.id,
+                        'rawContent': tweet.rawContent,
+                        'username': tweet.user.username})
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
+            result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
+            print(f"Matched {result.matched_count} documents.")
+            print(f"Modified {result.modified_count} documents.")
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        elif (target['targetType'] == 'twitter-hashtag'):
+            for keyword in eval(target['targets']):
+                tweets = []
+                snsScrapper = sntwitter.TwitterSearchScraper('#'+keyword)
+                for counter, tweet in enumerate(snsScrapper.get_items()):
+                    if counter > 10:
+                        break
+                    tweets.append({
+                        'keyword': keyword,
+                        'date': tweet.date,
+                        'id': tweet.id,
+                        'rawContent': tweet.rawContent,
+                        'username': tweet.user.username})
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
+            result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
+            print(f"Matched {result.matched_count} documents.")
+            print(f"Modified {result.modified_count} documents.")
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+        elif (target['targetType'] == 'twitter-user'):
+            for keyword in eval(target['targets']):
+                tweets = []
+                snsScrapper = sntwitter.TwitterUserScraper(keyword)
+                for counter, tweet in enumerate(snsScrapper.get_items()):
+                    if counter > 10:
+                        break
+                    tweets.append({
+                        'keyword': keyword,
+                        'date': tweet.date,
+                        'id': tweet.id,
+                        'rawContent': tweet.rawContent,
+                        'username': tweet.user.username})
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            update = {'$set': {'status': 1}, '$push': {'tweets': {'$each': tweets}}}
+            result = db.targets.update_one({'_id': ObjectId(target['_id'])}, update)
+            print(f"Matched {result.matched_count} documents.")
+            print(f"Modified {result.modified_count} documents.")
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
